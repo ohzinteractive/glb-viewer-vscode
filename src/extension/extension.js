e16a70c337ed65f58f3bd25012e34150cb2895fc
Gianni Ballerini <Gianniballerini@gmail.com>
2025-05-15 10:51:48 -0300

    initial structure for vite and vs extension

---
+++ src/extension/extension.js
@@ -0,0 +1,35 @@
+const vscode = require('vscode');
+const fs = require('fs');
+const path = require('path');
+
+function activate(context) {
+  context.subscriptions.push(
+    vscode.window.registerWebviewPanelSerializer('customView', {
+      async deserializeWebviewPanel(panel) {
+        setupWebview(panel);
+      }
+    })
+  );
+
+  context.subscriptions.push(
+    vscode.commands.registerCommand('extension.openCustomWebview', () => {
+      const panel = vscode.window.createWebviewPanel('customView', 'Custom View', vscode.ViewColumn.One, {
+        enableScripts: true
+      });
+      setupWebview(panel);
+    })
+  );
+}
+
+function setupWebview(panel) {
+  const htmlPath = path.join(__dirname, '../../dist/webview/index.html');
+  const html = fs.readFileSync(htmlPath, 'utf8');
+  panel.webview.html = html;
+}
+
+function deactivate() {}
+
+module.exports = {
+  activate,
+  deactivate
+};
