eaf3a73a151dd7c47c6cc05b243e007c4d79415b
Gianni Ballerini <Gianniballerini@gmail.com>
2025-05-19 15:51:33 -0300

    Load three and add-ons locally

--- src/webview/SceneController.js
+++ src/webview/SceneController.js
@@ -1,6 +1,6 @@
-import * as THREE from 'three';
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { GLTFLoader } from './lib/three/GLTFLoader.js';
+import { OrbitControls } from './lib/three/OrbitControls.js';
+import * as THREE from './lib/three/three.module.js';
 
 class SceneController
 {
--- vite.config.mjs
+++ vite.config.mjs
@@ -9,12 +9,6 @@ export default defineConfig({
     emptyOutDir: true,
     rollupOptions: {
       input: 'src/webview/index.html',
-      external: [
-        'three',
-        'three/addons/loaders/GLTFLoader.js',
-        'three/examples/jsm/loaders/GLTFLoader.js',
-        'three/examples/jsm/controls/OrbitControls.js'
-      ],
       output: {
         entryFileNames: 'assets/[name].js',
         chunkFileNames: 'assets/[name].js',
