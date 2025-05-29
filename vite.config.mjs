a3191da34a198a07a2af8a8aa4724ecd08afa112
Federico Gratti <federicogratti@gmail.com>
2025-05-28 23:56:41 -0300

    Listen for pug changes

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,6 +1,61 @@
+import fs from 'fs';
+import path from 'path';
+import pug from 'pug';
 import { defineConfig } from 'vite';
-import vitePugPlugin from 'vite-plugin-pug-transformer';
 import packagejson from './package.json';
+
+const pugLocals = { package: packagejson };
+
+function watchPugFiles() {
+  return {
+    name: 'watch-all-pug',
+    buildStart() {
+      // Recursively add all .pug files in your src/webview directory
+      const walk = dir => {
+        fs.readdirSync(dir).forEach(file => {
+          const fullPath = path.join(dir, file);
+          if (fs.statSync(fullPath).isDirectory()) {
+            walk(fullPath);
+          } else if (file.endsWith('.pug')) {
+            this.addWatchFile(fullPath);
+          }
+        });
+      };
+      walk(path.resolve(__dirname, 'src/webview'));
+    }
+  };
+}
+
+function compilePugToHtml() {
+
+  const templatePath = path.resolve(__dirname, 'src/webview/index.pug');
+  const htmlPath = path.resolve(__dirname, 'src/webview/index.html');
+
+  return {
+    name: 'compile-index-pug',
+    buildStart() {
+      this.addWatchFile(templatePath);
+    },
+    async generateBundle() {
+      const html = pug.renderFile(templatePath, {
+        pretty: true,
+        package: require('./package.json')
+      });
+
+      let currentHtml = '';
+      try {
+        currentHtml = fs.readFileSync(htmlPath, 'utf-8');
+      } catch (e) {
+        // File doesn't exist yet
+      }
+
+      if (html !== currentHtml) {
+        fs.writeFileSync(htmlPath, html, 'utf-8');
+        console.log('[vite-plugin] Updated index.html from index.pug');
+      }
+    }
+  };
+}
 
 export default defineConfig({
   root: 'src/webview',
@@ -33,11 +88,8 @@ export default defineConfig({
     }
   },
   plugins: [
-    vitePugPlugin({
-      pugLocals: {
-        package: packagejson
-      }
-    })
+    compilePugToHtml(),
+    watchPugFiles(),
   ],
   css: {
     preprocessorOptions: {
