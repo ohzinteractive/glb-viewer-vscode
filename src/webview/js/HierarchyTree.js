aa9437cfdbbec36f36198914d1ec4524c3625425
Ramiro Fages <fages.fages@gmail.com>
2025-06-14 23:43:23 -0300

    Fix hierarchy icons

--- src/webview/js/HierarchyTree.js
+++ src/webview/js/HierarchyTree.js
@@ -44,10 +44,11 @@ class HierarchyTree extends ResizableWindow
     const ICON_OPEN_EYE = '<svg fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"> <path d="M0 7h2v2H0V7zm4 4H2V9h2v2zm4 2v-2H4v2H2v2h2v-2h4zm8 0H8v2H6v2h2v-2h8v2h2v-2h-2v-2zm4-2h-4v2h4v2h2v-2h-2v-2zm2-2v2h-2V9h2zm0 0V7h2v2h-2z" fill="currentColor"/> </svg>';
     const ICON_CLOSED_EYE = '<svg fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"> <path d="M8 6h8v2H8V6zm-4 4V8h4v2H4zm-2 2v-2h2v2H2zm0 2v-2H0v2h2zm2 2H2v-2h2v2zm4 2H4v-2h4v2zm8 0v2H8v-2h8zm4-2v2h-4v-2h4zm2-2v2h-2v-2h2zm0-2h2v2h-2v-2zm-2-2h2v2h-2v-2zm0 0V8h-4v2h4zm-10 1h4v4h-4v-4z" fill="currentColor"/> </svg>';
 
-    const $node = document.createElement('div');
-    $node.dataset.depth = depth;
-    $node.dataset.is_geometry = object3d.geometry !== undefined;
-    $node.className = 'tree-node';
+    const $node                   = document.createElement('div');
+    $node.dataset.depth           = depth;
+    $node.dataset.is_geometry     = object3d.geometry !== undefined;
+    $node.dataset.is_empty_object = object3d.geometry === undefined && object3d.children.length === 0;
+    $node.className               = 'tree-node';
     // depth 0 -> first level
     if (depth === 0)
     {
@@ -192,7 +193,7 @@ class HierarchyTree extends ResizableWindow
     const children_node = node.querySelector('.tree-node__children');
     children_node.style.display = 'block';
 
-    if (node.dataset.is_geometry === 'false')
+    if (node.dataset.is_geometry === 'false' && node.dataset.is_empty_object === 'false')
     {
       console.log('no es geometry');
       wrapper.querySelector('.tree-node__icon').innerHTML = ICON_ARROW_DOWN;
